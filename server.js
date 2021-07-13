b1f233cad8dae9f6fa421c6e5a26c5accd6c4e64
Derek Phelps <derekphtpt@gmail.com>
2021-07-12 22:12:25 -0600

    express methods

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const { query } = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }))
+// parse incoming JSON data
+app.use(express.json());
 
 
 function filterByQuery(query, animalsArray) {
@@ -67,6 +71,11 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
