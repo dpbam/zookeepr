73e194b84376ac2cd33b1211b3e0ebf1b9d8a423
Derek Phelps <derekphtpt@gmail.com>
2021-07-14 15:21:58 -0600

    11.3.5 added fetch to api/animals

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -105,17 +107,22 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
-
+    console.log(req.body)
     // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
         res.status(400).send('The animal is not properly formatted.');
-    } else {}
+    } else {
     // add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
+        const animal = createNewAnimal(req.body, animals);
 
     // req.body is where our incoming content will be
-    res.json(animal);
+        res.json(animal);
+    }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
